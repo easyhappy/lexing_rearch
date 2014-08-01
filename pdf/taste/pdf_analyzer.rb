cbb75344c1424a3ee6ee6034394137fbb21a2aae
AndyHu <meeasyhappy@gmail.com>
2014-08-01 09:15:21 +0800

    添加pdf_analyzer

--- pdf/taste/pdf_analyzer.rb
+++ pdf/taste/pdf_analyzer.rb
@@ -1,4 +1,25 @@
 require './page_analyzer'
+require 'yaml'
+require 'pry'
 
 class PdfAnalyzer
+  def initialze(file_name)
+    @filename = filename
+    load_configs
+  end
+
+  def load_configs
+    @configs = YAML::load(File.open('pdf_config.yml'))
+    binding.pry
+  end
+
+  def run
+    fetch_pdf_catalogs
+  end
+
+  def fetch_pdf_catalogs
+
+  end
 end
+
+analyzer = PdfAnalyzer.new 'Audi+A4L+B8_cn.pdf'
