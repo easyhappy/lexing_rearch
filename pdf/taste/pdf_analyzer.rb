57b1cdeb3bf6fc1a2ebaf16dcec768ebca494634
AndyHu <meeasyhappy@gmail.com>
2014-08-01 09:26:37 +0800

    No message.

--- pdf/taste/pdf_analyzer.rb
+++ pdf/taste/pdf_analyzer.rb
@@ -3,14 +3,14 @@ require 'yaml'
 require 'pry'
 
 class PdfAnalyzer
-  def initialze(file_name)
-    @filename = filename
+  def initialize(file_name)
+    @file_name = file_name
     load_configs
   end
 
   def load_configs
-    @configs = YAML::load(File.open('pdf_config.yml'))
-    binding.pry
+    @all_configs = YAML::load(File.open('pdf_config.yml'))
+    @file_configs = @all_configs[@file_name] || @all_configs['default']
   end
 
   def run
